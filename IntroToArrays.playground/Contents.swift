1bffa5e32af26b016f469fff63659a3c0d2a65d0
cazlito <142947671+cazlito@users.noreply.github.com>
2023-09-26 13:28:26 -1000

    did parts 1-8

--- IntroToArrays.playground/Contents.swift
+++ IntroToArrays.playground/Contents.swift
@@ -1,3 +1,32 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+//part 3
+//declare an array named "fruitNames" and it is going to hold objects of type string
+
+var fruitNames: [String]
+
+//part 4
+fruitNames = ["Kiwi", "Apple", "Watermelon"]
+
+//part 5
+print(fruitNames)
+
+//part 6
+//print "I like to eat Kiwi"
+
+print("I like to eat \(fruitNames)")
+
+//part 7
+for name in fruitNames {
+    print("I like to eat " + name)
+}
+
+//part 8
+//index starts at 0 instead of 1
+print("I like to eat \(fruitNames[2])")
+print(fruitNames[1])
+
+
+
